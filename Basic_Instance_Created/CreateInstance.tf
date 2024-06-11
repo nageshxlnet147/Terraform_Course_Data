c28f044fe53a2616a8f10457c392ab573df83894
nageshxlnet147 <nageshxlnet147@gmail.com>
2024-06-12 02:51:41 +0530

    Day5 notes added

--- Basic_Instance_Created/CreateInstance.tf
+++ Basic_Instance_Created/CreateInstance.tf
@@ -1,12 +1,9 @@
-provider "aws" {
-    access_key = "ACCESS_KEY"
-    secret_key = "SECRET_KEY"
-    region = "us-east-2"
-  
-}
-
 resource "aws_instance" "MyFirstInstant" {
+    count = 3
     ami = "ami-05803413c51f242b7"
     instance_type = "t2.micro"
-}
 
+    tags = {
+      Name = "Instance-${count.index}"
+    }
+}
