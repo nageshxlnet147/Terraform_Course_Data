5de44f340e86135fcccdb6937b40badd8c40dd85
nageshxlnet147 <nageshxlnet147@gmail.com>
2024-06-20 20:39:11 +0530

    Day8 notes and codes added

---
+++ Terraform_concepts_Basic_bulding_blocks/Demo_DataSource/provider.tf
@@ -0,0 +1,16 @@
+# provider "aws" {
+#     access_key = "${var.AWS_ACCESS_KEY}"
+#     secret_key = "${var.AWS_SECRET_KEY}"
+#     region = "${var.AWS_REGION}"
+#                 }
+
+
+##################################### day7
+
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
+}
+
+####################################
